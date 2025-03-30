1b3dde0cd5a7e64fe3b39ee1c832a23e45a2f9c0
Davranbek <kadirimbetov@arizona.edu>
2025-03-29 21:45:24 -0700

    Update AccountStorage.java
    might be added some other functions and a parameter for a constructor.

--- projectFinal/src/backend/AccountStorage.java
+++ projectFinal/src/backend/AccountStorage.java
@@ -4,4 +4,20 @@ import java.util.HashMap;
 
 public class AccountStorage {
 	private HashMap<Integer, User> accountList;
+	
+	public AccountStorage() {
+		accountList = new HashMap<Integer, User>();
+	}
+	
+	public void createAccount() {
+		// creates an account
+	}
+	
+	public void logIntoAccount() {
+		// log in to an existiog account
+	}
+	
+	public void hashPassword() {
+		// uses security to hash the password
+	}
 }
