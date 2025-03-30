3c6142203b4fa71e243263cd189e7e707621b53e
Davranbek <kadirimbetov@arizona.edu>
2025-03-29 21:49:00 -0700

    Update Course.java
    not final structure. needs a lot of changes. we may need copy constructor.

--- projectFinal/src/backend/Course.java
+++ projectFinal/src/backend/Course.java
@@ -5,8 +5,22 @@ import java.util.HashMap;
 
 public class Course {
 	private HashMap<Integer, Assignment> assignmentList; // The integer is the hash of the Assignment.
+	private String courseName;
 	// No duplicate assignments! ... Why would you have a duplicate, anyways?
 	private HashMap<String, Student> studentList; // The string is the username
 	private Boolean completed;
 	private HashMap<String, ArrayList<Student>> groupList; // String is "group name"
+	
+	public Course(String courseName) {
+		this.courseName = courseName;
+		assignmentList = new HashMap<Integer, Assignment>();
+		studentList = new HashMap<String, Student>();
+		groupList = new HashMap<String, ArrayList<Student>>();
+	}
+	
+	public String getCourseName() {
+		return courseName;
+	}
+	
+	// there might be other getters it depends on the functionality of Model methods
 }
