61359b2b54bed3e3fbe8da644401a965787b02d1
Davranbek <kadirimbetov@arizona.edu>
2025-03-29 21:50:55 -0700

    Update User.java
    we may add getUsername and getCourse method in here

--- projectFinal/src/backend/User.java
+++ projectFinal/src/backend/User.java
@@ -4,6 +4,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class User { // User is both student & teacher - use this for methods used by both.
-	private String usernames;
-	private HashSet<Course> courseList; // Courses will have a hash; does not change as edited
+	protected String username;
+	protected HashSet<Course> courseList; // Courses will have a hash; does not change as edited
 }
