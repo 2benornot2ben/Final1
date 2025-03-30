8494b7e14af9fa6b2e1bff1ec55780eb902d4dbd
Davranbek <kadirimbetov@arizona.edu>
2025-03-29 21:49:55 -0700

    Update Student.java
    not final structure

--- projectFinal/src/backend/Student.java
+++ projectFinal/src/backend/Student.java
@@ -6,7 +6,26 @@ import java.util.HashSet;
 public class Student extends User {
 	private String firstName;
 	private String lastName;
-	private HashMap<String, Integer> studentAverageGrades; // String is meant to be classes
+	private HashMap<String, Double> studentAverageGrades; // String is meant to be classes
 	private enum Grades {A, B, C, D, E, F};
 	private HashMap<String, Grades> studentGradeLetters; // Sadly not automatically updated
+	
+	public Student(String first, String last, String user) {
+		this.firstName = first;
+		this.lastName = last;
+		username = user;
+		courseList = new HashSet<Course>();
+	}
+	
+	public String getFirstName(){
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public HashSet<Course> getCourseList(){
+		//this should return copy of course list
+	}
 }
