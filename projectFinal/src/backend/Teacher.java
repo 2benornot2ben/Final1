d113bd84d5836ef1f0abd2d156dcf8c82240dcf5
Davranbek <kadirimbetov@arizona.edu>
2025-03-29 21:50:20 -0700

    Update Teacher.java
    not final structure.

--- projectFinal/src/backend/Teacher.java
+++ projectFinal/src/backend/Teacher.java
@@ -3,5 +3,12 @@ package backend;
 import java.util.HashSet;
 
 public class Teacher extends User {
+	public Teacher(String user) {
+		username = user;
+		courseList = new HashSet<Course>();
+	}
 	
+	public HashSet<Course> getCourseList(){
+		//this should return copy of course list
+	}
 }
