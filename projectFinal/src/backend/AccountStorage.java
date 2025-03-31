6d59199ed5bf12403b22a32860287b88659ebafe
Fatih Bozdogan <bozdoganf05@gmail.com>
2025-03-31 12:11:52 -0700

    Update AccountStorage.java

--- projectFinal/src/backend/AccountStorage.java
+++ projectFinal/src/backend/AccountStorage.java
@@ -4,4 +4,5 @@ import java.util.HashMap;
 
 public class AccountStorage {
 	private HashMap<Integer, User> accountList;
+	// make this a hashmap<String, User> where the string represents the username
 }
