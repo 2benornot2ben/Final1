49bc7121035a329a5c30b19cdab1e2ae28c942eb
Davranbek <kadirimbetov@arizona.edu>
2025-04-20 21:31:02 -0700

    Update Course.java

--- projectFinal/src/backend/Course.java
+++ projectFinal/src/backend/Course.java
@@ -6,13 +6,15 @@ import java.util.HashSet;
 import java.util.Iterator;
 
 public class Course {
-	private HashMap<String, Assignment> assignmentMap; // I don't think we need the hash of the assignment. Each assignment has a unique name.
+	private HashMap<String, Assignment> assignmentMap; // str is the assignment name : Assignment obj
 	private String courseName;
-	
+	private HashMap<String, FinalGrade> finalGrades;
 	private Teacher teacher; // Comment: Should each course have a teacher?
 	private HashMap<String, Student> studentMap; // The string is the username
 	private Boolean completed;
 	private HashMap<String, ArrayList<Student>> groupList; // String is "group name"
+	private double totalGrade;
+	private HashMap<AssignmentType,Double> categories; // Assignment Type : Weight of that
 	
 	public Course(String courseName) {
 		this.completed = false;
@@ -21,16 +23,30 @@ public class Course {
 		studentMap = new HashMap<String, Student>();
 		groupList = new HashMap<String, ArrayList<Student>>(); // This could be an arraylist if you think
 		// groups should be ordered by "number". We are contractually obligated to consider this.
+		
+		categories = new HashMap<>();
+        categories.put(AssignmentType.MIDTERM, 300.0);
+        categories.put(AssignmentType.FINAL,   200.0);
+        categories.put(AssignmentType.QUIZ,    125.0);
+        categories.put(AssignmentType.HW,      125.0);
+        categories.put(AssignmentType.PROJECT, 250.0);
 	}
 	
+	protected void setTotalGrade(double total) {
+		totalGrade = total;
+	}
 	// This is meant to be used for IMPORTING.
 	protected Course(String courseName, HashMap<String, Assignment> assignmentMap,
-			HashMap<String, Student> studentMap, HashMap<String, ArrayList<Student>> groupMap) {
+			HashMap<String, Student> studentMap, HashMap<String, ArrayList<Student>> groupMap) { 
 		this.completed = false; // May be changed later
 		this.courseName = courseName;
 		this.assignmentMap = assignmentMap; // escaping reference; will handle it later when we actually use it.
 		this.studentMap = studentMap; // escaping reference; will handle it later when we actually use it.
 		this.groupList = groupMap; // escaping reference; will handle it later when we actually use it.
+	}
+	
+	protected void setFinalGrades(HashMap<String, FinalGrade> finalGrades) {
+		this.finalGrades = finalGrades;
 	}
 	
 	public String getCourseName() {
@@ -41,10 +57,14 @@ public class Course {
 		this.studentMap = studentMap;
 	}
 	
+	protected void setAssignmentMap(HashMap<String, Assignment> assignmentMap) {
+		this.assignmentMap = assignmentMap;
+	}
+	
 	public void setTeacher(Teacher teacher) {
 		this.teacher = teacher;
 	}
-	
+		
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj) return true;
@@ -103,7 +123,7 @@ public class Course {
 	}
 	
 	protected HashMap<String, Assignment> getAssignmentsMap(){
-		return new HashMap<String, Assignment>(this.assignmentMap);
+		return assignmentMap;
 	}
   
   /*
@@ -151,4 +171,31 @@ public class Course {
 	// public double getStudentAverage(String studentUsername) {
 	// 	return studentMap.get(studentUsername).getStudentAverage(courseName);
 	// }
+
+	public void addAssignment(String assignmentName, String assignmentCategory) {
+		Assignment newAssignment = new Assignment(assignmentName,this.convertToEnums(assignmentCategory));
+		newAssignment.setCategory(this.convertToEnums(assignmentCategory));
+		assignmentMap.put(assignmentName, newAssignment);
+	}
+	
+	public void removeAssignment(String assignmentName, String assignmentCategory) {
+		assignmentMap.remove(assignmentName);
+	}
+	
+	private AssignmentType convertToEnums(String category) {
+        switch (category.toLowerCase()) {
+	        case "midterm":
+	            return AssignmentType.MIDTERM;
+	        case "final":
+	            return AssignmentType.FINAL;
+	        case "quiz":
+	            return AssignmentType.QUIZ;
+	        case "hw":
+	            return AssignmentType.HW;
+	        case "project":
+	            return AssignmentType.PROJECT;
+	        default:
+	            throw new IllegalArgumentException("Unknown assignment type: " + category);
+        }
+	}
 }
