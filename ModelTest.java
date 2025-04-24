9913cb69930a3a2d3df4736c6658571a98e22de4
Fatih Bozdogan <bozdoganf05@gmail.com>
2025-04-23 18:57:33 -0700

    Add files via upload

--- ModelTest.java
+++ ModelTest.java
@@ -44,9 +44,57 @@ public class ModelTest {
 		model.calculateClassAverage(0, "CSC252", weights, drops);
 		model.checkCompleted("CSC252");
 		model.calculateStats("CSC252", "midterm");
-		// model.calculateGPA("dlee");
-		// model.calculateStudentCurAverage("dlee");
-		// model.getEnrolledStudents("CSC252");
+		model.calculateGPA("dlee");
+		model.calculateStudentCurAverage("dlee");
+		model.getEnrolledStudents("CSC252");
+		model.removeAssignment("CSC252", "syllabus_quiz", "quiz");
+		model.canCalculateStudentCurAverage("CSC252", "dlee");
+		model.toString();
+		model.sortByFirstName("CSC252");
+		model.sortByGrades("CSC252", "syllabus_quiz");
+		model.sortByGrades("CSC252", "Hw1");
+		model.sortByLastName("CSC252");
+		model.sortByUserName("CSC252");
+		model.setAssignmentGraded("CSC252", "Hw1");
+		model.canCalculateStudentCurAverage("CSC252", "dlee");
+		model.checkCompleted("CSC252");
+		model.completeCourse("CSC252");
+		model.equals(drops);
+		model.equals(model);
+		model.equals(null);
+		model.getCurCoursesStudent("dlee");
+		model.getCurCoursesTeacher("jjohnson");
+		model.getIsTeacher();
+		model.isSetUp("CSC252");
+		model.getStudentGrade("dlee", "CSC252", "Hw1");
+		model.getUngradedAssignments("CSC252");
+		model.getUngradedAssignmentsUSER("CSC252", "dlee");
+		model.getStudentGrade("dlee", "CSC252", "Hw1");
+		
+		model.canCreateGroups("CSC252", 2);
+		model.canCreateGroups("CSC252", 20);
+		
+		model.putInGroups("CSC252", 2);
+
+		model.addAssignment("CSC252", "Hw1", "HW");
+		model.removeAssignment("CSC252", "Hw1", "HW");
+		
+		model.getStudentList("CSC252");
+		
+		model.getGradedAssignmentsUSER("CSC252", "dlee");
+
+		model.getCourseAverage("CSC252", "dlee");
+
+		model.removeStudent("sgupta", "CSC252");
+		
+		model.removeStudent("devbehruz", "CSC252");
+		
+		model.removeStudent("lwilson", "CSC252");
+		
+		model.addStudent("sgupta", "CSC252");
+		
+		model.canCalculateStudentCurAverage("CSC252", "sgupta");
+
 
 	}
 
